54c4993a0735a9e2ac37be166a1549bc89ef6be0
michaelkunc <mjkunc@gmail.com>
2015-09-03 18:51:07 -0500

    keyboard is generating notes

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 console.log('is this thing on?');
 
+var context = new AudioContext(),
+  masterVolume = context.createGain();
+
+  masterVolume.connect(context.destination);
+
+
 // requesting midi access
 if (navigator.requestMIDIAccess) {
     navigator.requestMIDIAccess({
@@ -10,7 +16,7 @@ if (navigator.requestMIDIAccess) {
 }
 
 function onMIDISuccess(midiAccess){
-  console.log('MIDI access object', midiAccess);
+  // console.log('MIDI access object', midiAccess);
 
   midi = midiAccess;
   var inputs = midi.inputs.values();
@@ -24,35 +30,36 @@ function onMIDIMessage(event){
   data = event.data,
   cmd = data[0] >> 4,
   channel = data[0] & 0xf,
-  type = data[0] & 0xf,
+  type = data[0],
   note  = data[1],
   velocity = data[2];
 
-  switch (type) {
-    case 144:
-      noteOn(note,velocity);
-      break;
-    case 128:
-      noteOff(note, velocity);
-      break;
+  if (type == 144) {
+    noteOn(note, velocity);
+  } else {
+    noteOff(note, velocity);
   }
-
 
   console.log('MIDI data', data);
 
 }
 
-function frequencyFromNoteNumber(note){
-  return 440 * Math.pow(2, (note-69) / 12);
-}
+// function frequencyFromNoteNumber(note){
+//   return 440 * Math.pow(2, (note-69) / 12);
+// }
 
-
+// will eventually want to create a new osc object right now I am binding the osc to the window. which is
+// why I can't play chords
 function noteOn(midiNote, velocity){
-  player(midiNote, velocity);
+  this.osc = context.createOscillator();
+  this.osc.type = 'sine';
+
+  this.osc.connect(context.destination);
+  this.osc.start(context.currentTime);
 }
 
 function noteOff(midiNote, velocity){
-  player(midiNote, velocity);
+  this.osc.stop(context.currentTime);
 }
 
 
